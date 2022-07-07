31c66df8f2117c9223a2fb653d6813f6da53e3c4
Azamat <azamatduishenkulov711@gmail.com>
2022-07-07 12:29:49 +0600

    finish

--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -5,25 +5,11 @@ function Features() {
     return (
         <div>
             <div className="feature">
-            <div className="container">
-            <h2 className="section-title">Features</h2>
-            <ul className="feature-list">
-                <li className="feature-item">
-                <img src="images/card-img1.png" alt="" className="feature-image" />
-                <div className="feature-info">
-                    <h3 className="feature-title">
-                    Supports All Currencies and Cards
-                    </h3>
-                    <p className="feature-text">
-                    We support all popular currencies and is fully customizable to add
-                    </p>
-                    <a href="#" className="feature-link">
-                    Read More
-                    </a>
-                </div>
-                </li>
-                <li className="feature-item">
-                    <img src="images/card-img2.png" alt="" className="feature-image" />
+                <div className="contoiner">
+                <h2 className="section-title">Features</h2>
+                <ul className="feature-list">
+                    <li className="feature-item">
+                    <img src="images/card-img1.png" alt="" className="feature-image" />
                     <div className="feature-info">
                         <h3 className="feature-title">
                         Supports All Currencies and Cards
@@ -35,24 +21,38 @@ function Features() {
                         Read More
                         </a>
                     </div>
-                </li>
-                <li className="feature-item">
-                <img src="images/card-img3.png" alt="" className="feature-image" />
-                <div className="feature-info">
-                    <h3 className="feature-title">
-                    Supports All Currencies and Cards
-                    </h3>
-                    <p className="feature-text">
-                    We support all popular currencies and is fully customizable to add
-                    </p>
-                    <a href="#" className="feature-link">
-                    Read More
-                    </a>
+                    </li>
+                    <li className="feature-item">
+                        <img src="images/card-img2.png" alt="" className="feature-image" />
+                        <div className="feature-info">
+                            <h3 className="feature-title">
+                            Supports All Currencies and Cards
+                            </h3>
+                            <p className="feature-text">
+                            We support all popular currencies and is fully customizable to add
+                            </p>
+                            <a href="#" className="feature-link">
+                            Read More
+                            </a>
+                        </div>
+                    </li>
+                    <li className="feature-item">
+                    <img src="images/card-img3.png" alt="" className="feature-image" />
+                    <div className="feature-info">
+                        <h3 className="feature-title">
+                        Supports All Currencies and Cards
+                        </h3>
+                        <p className="feature-text">
+                        We support all popular currencies and is fully customizable to add
+                        </p>
+                        <a href="#" className="feature-link">
+                        Read More
+                        </a>
+                    </div>
+                    </li>
+                </ul>
                 </div>
-                </li>
-            </ul>
             </div>
-        </div>
         </div>
     )
 }
